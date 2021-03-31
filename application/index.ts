ab65eadf15a91a1bc695f0666322f6b283ae3dd9
Stijn de Witt <stijndewitt@hotmail.com>
2021-04-01 00:10:15 +0200

    Testing whether there are multiple anylogger instances floating around

--- application/index.ts
+++ application/index.ts
@@ -5,6 +5,11 @@ import { log } from 'myLib';
 // The instance of anylogger in myLib doesn't seem to be affected by any adapters
 log('hello from myLib log in application');
 
+
 // To demonstrate `anylogger-console` working as expected from application
 const applog = anylogger('application');
 applog('hello directly from applog in application');
+
+applog('log.name=', log.name)
+applog('log.name in anylogger', log.name in anylogger)
+applog('anylogger.justTesting=', (anylogger as any).justTesting)
--- myLib/src/helpers/logging.ts
+++ myLib/src/helpers/logging.ts
@@ -1,5 +1,7 @@
 import anylogger from 'anylogger';
+import { isConstructorDeclaration } from 'typescript';
 
+(anylogger as any).justTesting = 'Let\'s try something';
 const log = anylogger('myLib');
-
+console.info('anylogger.justTesting=', (anylogger as any).justTesting)
 export default log;
