9b996a368a685af42fb9f40107c8eff4101795fe
Stijn de Witt <stijndewitt@hotmail.com>
2021-04-03 15:19:27 +0200

    Removed the logging I added in the previous PR again to make for a cleaner commit Upgraded myLib to 1.0.1 so I could publish again

--- application/index.ts
+++ application/index.ts
@@ -9,7 +9,3 @@ log('hello from myLib log in application');
 // To demonstrate `anylogger-console` working as expected from application
 const applog = anylogger('application');
 applog('hello directly from applog in application');
-
-applog('log.name=', log.name)
-applog('log.name in anylogger', log.name in anylogger)
-applog('anylogger.justTesting=', (anylogger as any).justTesting)
--- myLib/src/helpers/logging.ts
+++ myLib/src/helpers/logging.ts
@@ -1,7 +1,5 @@
 import anylogger from 'anylogger';
-import { isConstructorDeclaration } from 'typescript';
 
-(anylogger as any).justTesting = 'Let\'s try something';
 const log = anylogger('myLib');
-console.info('anylogger.justTesting=', (anylogger as any).justTesting)
+
 export default log;
